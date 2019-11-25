d7d5ce0691243f88f84e9a604d639d963c7671de
Bartlomiej Boruta <diabeuboruta@gmail.com>
2019-11-25 21:47:17 +0100

    Added assertion to BingSearchTest.

--- src/test/java/przyklad1/BingSearchTest.java
+++ src/test/java/przyklad1/BingSearchTest.java
@@ -7,26 +7,23 @@ import org.openqa.selenium.By;
 import org.junit.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SearchTest {
+public class BingSearchTest {
 
     private WebDriver driver;
+
+    private String phrase = "Mistrzostwa Świata w piłce noznej 2018";
 
     @Before
     public void setUp() {
 
-//        System.setProperty("webdriver.chrome.driver",
-//                "src/main/resources/drivers/chromedriver.exe");
-//        // Uruchom nowy egzemplarz przeglądarki Chrome
-//        driver = new ChromeDriver();
-
-        System.setProperty("webdriver.gecko.driver",
-                "src/main/resources/drivers/geckodriver.exe");
-
-        driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver",
+                "src/main/resources/drivers/chromedriver.exe");
+        // Uruchom nowy egzemplarz przeglądarki Chrome
+        driver = new ChromeDriver();
         // Zmaksymalizuj okno przeglądarki
         driver.manage().window().maximize();
         // Przejdź do Google
-        driver.get("http://www.google.com");
+        driver.get("http://www.bing.com/");
     }
 
     @Test
@@ -36,13 +33,17 @@ public class SearchTest {
         // Wyczyść teskst zapisany w elemencie
         element.clear();
         // Wpisz informacje do wyszukania
-        element.sendKeys("Testowanie Selenium WebDriver");
+        element.sendKeys(phrase);
         // Prześlij formularz
         element.submit();
     }
+
     @After
     public void tearDown() throws Exception {
         // Zamknij przeglądarkę
+        String title = driver.getTitle();
+
+        Assert.assertTrue(title.contains(phrase));
         driver.quit();
     }
 }
